3b0df7024087a6249bedc3f82b471c452e1a04d6
cyc60 <avsysoev60@gmail.com>
2020-02-19 21:42:32 +0300

    CI: Remove sandbox branch

--- .circleci/init.sh
+++ .circleci/init.sh
@@ -8,12 +8,6 @@ export GCLOUD_PROJECT=videocoin-network
 export ENV=snb
 export CLUSTER=snb
 
-if [ "$CIRCLE_BRANCH" == "sandbox" ]; then
-    export GCLOUD_PROJECT=videocoin-network
-    export ENV=snb
-    export CLUSTER=snb
-fi
-
 if [ "$CIRCLE_BRANCH" == "develop" ]; then
     export GCLOUD_PROJECT=videocoin-network
     export ENV=dev
